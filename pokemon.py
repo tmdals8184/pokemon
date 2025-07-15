ddc8a10ee457b2dab2afa6bd3aec3f5a66b5becb
LeeSeungmin <102977104+tmdals8184@users.noreply.github.com>
2025-07-15 14:32:13 +0900

    Create pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,41 @@
+# Github Action
+# 1. py
+# 2. 어떤 OS에서 동작?? ubuntu
+# 3. 크롤링 - Selenium
+#  - 브라우저를 켜지 않고도 할 수 있는가?
+#  - linux 크롬 드라이버
+
+import selenium.webdriver as wb
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_argument("--headless") # 창 없음
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+
+url = "https://pokemonkorea.co.kr/pokedex"
+driver = wb.Chrome(options=options)
+driver.maximize_window()
+driver.get(url)
+
+img = driver.find_element(By.CSS_SELECTOR, "img.img-fluid")
+img.click()
+time.sleep(1)
+h3 = driver.find_element(By.TAG_NAME, "h3")
+no, name = h3.text.split()[1:]
+
+import csv
+import os
+pokemon_exist = os.path.exists("pokemon.csv")
+header = ["no", "name"]
+
+with open("pokemon.csv", "a") as file:
+    writer = csv.writer(file)
+    if not pokemon_exist:
+        writer.writerow(header)
+    writer.writerow([no, name])
+    print("포켓몬 저장 완료!!")
